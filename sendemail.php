8614641372a68573bb79ad16d73c6cf7c5aa9aa1
Son Nguyen Hoang <124531104+hoangsonww@users.noreply.github.com>
2023-10-21 16:56:13 -0400

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -16,3 +16,49 @@
 
     }
 ?>
+        
+<?php
+$host = 'localhost';
+$db   = 'portfolio';
+$user = 'username';
+$pass = 'password';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+function getArticles($pdo) {
+    $stmt = $pdo->query('SELECT * FROM articles');
+    return $stmt->fetchAll();
+}
+
+function getArticle($pdo, $id) {
+    $stmt = $pdo->prepare('SELECT * FROM articles WHERE id = ?');
+    $stmt->execute([$id]);
+    return $stmt->fetch();
+}
+
+function createArticle($pdo, $title, $content) {
+    $stmt = $pdo->prepare('INSERT INTO articles (title, content) VALUES (?, ?)');
+    $stmt->execute([$title, $content]);
+}
+
+function updateArticle($pdo, $id, $title, $content) {
+    $stmt = $pdo->prepare('UPDATE articles SET title = ?, content = ? WHERE id = ?');
+    $stmt->execute([$title, $content, $id]);
+}
+
+function deleteArticle($pdo, $id) {
+    $stmt = $pdo->prepare('DELETE FROM articles WHERE id = ?');
+    $stmt->execute([$id]);
+}
