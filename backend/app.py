502ee6e059b01dfe59397f85664f7391001ee46d
Son Nguyen <hoangson091104@gmail.com>
2024-09-01 09:58:27 -0400

    Update: Update profile information

--- backend/app.py
+++ backend/app.py
@@ -130,6 +130,7 @@ def get_skills():
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
